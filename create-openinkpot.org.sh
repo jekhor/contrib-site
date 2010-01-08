4c12cec7d52d9ae74addf33fbad6eaea35dac048
Igor Stroh <jenner@web.rulim.de>
2010-01-09 00:22:54 +0100

    changed path to hyde package

--- create-openinkpot.org.sh
+++ create-openinkpot.org.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # generates openinkpot.org site
-HYDE_DIR="./hyde"
+HYDE_DIR="./oi-hyde"
 SITE_DIR="./openinkpot.org"
-DEPLOY_DIR="/var/www/openinkpot.org"
-python "$HYDE_DIR" -g -s "$SITE_DIR" -d "$DEPLOY_DIR"
+#DEPLOY_DIR="/var/www/openinkpot.org"
+DEPLOY_DIR="/home/jenner/oi/oi-deploy"
+python "$HYDE_DIR/hyde.py" -g -s "$SITE_DIR" -d "$DEPLOY_DIR"
